42213ff06051004fd811d6de09f2afb1f2f1a2c0
yichao <1026290752@qq.com>
2018-04-01 01:37:40 +0800

    first commit

---
+++ src/main/java/cn/yichao/security/app/YichaoAuthotizationServerConfig.java
@@ -0,0 +1,11 @@
+package cn.yichao.security.app;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@Configuration
+//认证服务器
+@EnableAuthorizationServer
+public class YichaoAuthotizationServerConfig {
+
+}
